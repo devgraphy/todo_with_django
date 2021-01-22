2dc06279f3b6fef4d820496f0a73bb1e4052c676
Changjun Kim <beaican530@gmail.com>
2021-01-22 18:37:57 +0900

    deleted trash

--- TodoList/TodoList/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-# 어떤 액션에 어떤 view의 어떤 template을 호출할 것인지
-urlpatterns = [ 
-    # path('admin/', admin.site.urls),
-    path('',include('todo_app.urls'))   #아무 것도 없이 들어올 때 # 해당 dir에 파일없으니 생성 필요
-
-]
